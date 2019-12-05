27a423dd6d3fd43559360d02ba95490c69e98d86
liampancho <52308415+liampancho@users.noreply.github.com>
2019-12-06 01:18:16 +0800

    Update statisticalResult.java

--- statisticalResult.java
+++ statisticalResult.java
@@ -29,17 +29,16 @@ public class statisticalResult
         }
      }
      int total=letter+whitespace+digit+other;  
-     float letterPercent=(letter*100)/total;
-     float digitPercent=(digit*100)/total;
-     float whitespacePercent=(whitespace*100)/total;
-     float otherPercent=(other*100)/total;
-     float totalPercent=(total*100)/total;
+     float letterPercent=(float)(letter*100)/total;
+     float digitPercent=(float)(digit*100)/total;
+     float whitespacePercent=(float)(whitespace*100)/total;
+     float otherPercent=(float)(other*100)/total;
      System.out.println("\n"+text+"\n\nTotal Number of Characters in String: "+total);
      System.out.println("CATEGORY\t\t\tHOW MANY?\t\t\t% OF FILE");
      System.out.println("\nLETTERS\t\t\t\t"+letter+"\t\t\t\t"+letterPercent+"%");
      System.out.println("WHITESPACE\t\t\t"+whitespace+"\t\t\t\t"+whitespacePercent+"%");
      System.out.println("DIGITS\t\t\t\t"+digit+"\t\t\t\t"+digitPercent+"%");
      System.out.println("PUNCTUATIONS\t\t\t"+other+"\t\t\t\t"+otherPercent+"%");
-     System.out.println("TOTAL\t\t\t\t\t\t\t\t"+totalPercent+"%");
+     System.out.println("TOTAL\t\t\t\t\t\t\t\t100%");
    }
 }
