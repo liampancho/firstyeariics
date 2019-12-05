4a1f0ff5509152b709301987d8feda025c0e32c7
liampancho <52308415+liampancho@users.noreply.github.com>
2019-12-06 00:52:19 +0800

    Add files via upload

---
+++ statisticalResult.java
@@ -0,0 +1,45 @@
+import java.util.Scanner;
+public class statisticalResult
+{
+   public static void main(String[]args)
+   {
+     Scanner input=new Scanner(System.in);
+     String text;
+     System.out.print("Input any plain text: ");
+     text=input.nextLine();
+     char[]number=text.toCharArray();
+     int letter=0,digit=0,whitespace=0,other=0;     
+     for(int count=0;count<text.length();count++)
+     {
+        if(Character.isLetter(number[count]))
+        {
+          letter++;
+        }
+        else if(Character.isDigit(number[count]))
+        {
+          digit++;
+        }
+        else if(Character.isSpaceChar(number[count]))
+        {
+          whitespace++;
+        }
+        else
+        {
+          other++;
+        }
+     }
+     int total=letter+whitespace+digit+other;  
+     float letterPercent=(letter*100)/total;
+     float digitPercent=(digit*100)/total;
+     float whitespacePercent=(whitespace*100)/total;
+     float otherPercent=(other*100)/total;
+     float totalPercent=(total*100)/total;
+     System.out.println("\n"+text+"\n\nTotal Number of Characters in String: "+total);
+     System.out.println("CATEGORY\t\t\tHOW MANY?\t\t\t% OF FILE");
+     System.out.println("\nLETTERS\t\t\t\t"+letter+"\t\t\t\t"+letterPercent+"%");
+     System.out.println("WHITESPACE\t\t\t"+whitespace+"\t\t\t\t"+whitespacePercent+"%");
+     System.out.println("DIGITS\t\t\t\t"+digit+"\t\t\t\t"+digitPercent+"%");
+     System.out.println("PUNCTUATIONS\t\t\t"+other+"\t\t\t\t"+otherPercent+"%");
+     System.out.println("TOTAL\t\t\t\t\t\t\t\t"+totalPercent+"%");
+   }
+}
